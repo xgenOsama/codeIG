c15ef9b958e5aeb5f2b34c70e1683695ee80106a
gen <xgen.osama@gmail.com>
2015-06-03 08:28:12 +0200

    first commit

---
+++ application/models/post.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Post extends CI_Model{
+
+
+
+    function get_posts($num=20,$start=0){
+
+        //$sql = 'SELECT * FROM users WHERE active=1 ORDER BY data_added DESC  limit 0,20';
+        
+
+    }
+}
